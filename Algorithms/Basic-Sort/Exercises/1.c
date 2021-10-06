b7d5d0a57b663f054bcdc9d1f2085a4e0ab47648
lialittis <yu-tianchi@outlook.com>
2021-10-06 08:52:31 +0200

    add exercise for basic sort

---
+++ Algorithms/Basic-Sort/Exercises/1.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define LEN 9
+
+int nums[LEN] = {2,3,4,3,2,4,5,6,6};
+
+int main(int argc, char *argv[]) {
+  int target = 0;
+
+  for(int i = 0 ; i < LEN; i++){
+    target ^= nums[i];
+  }
+
+  printf("The target is %d.\n",target);
+
+  return 0;
+}
